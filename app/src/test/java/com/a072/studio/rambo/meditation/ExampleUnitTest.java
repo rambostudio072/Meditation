32d922b48f67d3d3802503609af393171617f9bf
rambostusio072 <rambostudio072@gmail.com>
2016-05-07 07:39:17 +0700

    test

---
+++ app/src/test/java/com/a072/studio/rambo/meditation/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.a072.studio.rambo.meditation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}

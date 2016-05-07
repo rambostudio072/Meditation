e28aae228c074d433d809fbaf4ce3bae22ca5da5
rambostusio072 <rambostudio072@gmail.com>
2016-05-07 07:33:51 +0700

    test

---
+++ app/src/main/java/com/a072/studio/rambo/meditation/MainActivity.java
@@ -0,0 +1,16 @@
+package com.a072.studio.rambo.meditation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView test;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
